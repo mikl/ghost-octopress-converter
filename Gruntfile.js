3d9e9fccc9004ba8cb65c183f2a04089c070f3e9
Mikkel Hoegh <mikkel@hoegh.org>
2014-01-25 16:23:49 +0100

    Never mind that.

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,27 +9,5 @@
 'use strict';
 
 module.exports = function(grunt) {
-  grunt.initConfig({
-    ghost_octopress_converter: {
-      options: {
-
-        // This is the standard address if you run npm start. Change it,
-        // if you want to post to a different Ghost installation.
-        url: 'http://127.0.0.1:2368/',
-
-        // The path where to send the posts for creation, will normally
-        // not need modification.
-        apiPath: 'ghost/api/v0.1/posts',
-
-        // If you want, you can define your Ghost credentials here. If not
-        // defined, you will be prompted to enter them before the import
-        // can proceed.
-        //username: 'ghostadmin',
-        //password: 'changeme',
-
-      }
-    }
-  });
-
   grunt.loadTasks('tasks');
 };
