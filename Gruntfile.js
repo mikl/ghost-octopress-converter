b0e0d39fe5eff4eb0ae94eee52f34d475e1fbfc2
Mikkel Hoegh <mikkel@hoegh.org>
2014-01-25 15:08:42 +0100

    Added simple Gruntfile for loading tasks.

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+/*
+ * ghost-octopress-converter
+ * https://github.com/mikl/ghost-octopress-converter
+ *
+ * Copyright (c) 2014 Mikkel Hoegh
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+  grunt.loadTasks('tasks');
+};
